d656589d0b3ad4ff50e2f0583d58c4aa6e8ae85d
jmrobinson <jmrobinsonma@gmail.com>
2020-04-16 18:47:59 -0400

    auto-commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
-from flask import render_template
+from flask import render_template,url_for
 from app import app
 
 @app.route('/')
-def mynotebook_page():
+@app.route('/home')
+def home():
+	return render_template('index.html')
+
+
+@app.route('/myscrapbook')
+def myscrapbook():
 	return render_template('mynotebook.html')
